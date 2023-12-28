9e30df33fb51bf0591e16bcd3c6dabd0601f1ca8
Sean Nance <sean_nance@hotmail.com>
2023-12-28 09:47:57 -0800

    created script.js file

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+Shopping List
+
+Create a shopping list in the variable myList.
+The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item.
+The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+
+There should be at least 5 sub-arrays in the list.
+*/
+
+const myList = [["Bread", 2.99], ["Milk", 3.99], ["Eggs", 4.99], ["Cheese", 6.99], ["Ham", 3.99]];
